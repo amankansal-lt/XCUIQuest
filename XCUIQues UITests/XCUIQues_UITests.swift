8d8ec4821c8bf25aa339a0213854209756cea9d4
amankansal-lt <155958106+amankansal-lt@users.noreply.github.com>
2024-10-29 10:33:28 +0530

    initial commit

---
+++ XCUIQues UITests/XCUIQues_UITests.swift
@@ -0,0 +1,24 @@
+import XCTest
+
+final class XCUIQues_UITestsClass: XCTestCase {
+    
+    func testExample1() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        Thread.sleep(forTimeInterval: 10)
+        
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testExample2() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        Thread.sleep(forTimeInterval: 5)
+        
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
